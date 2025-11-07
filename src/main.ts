305f3009d7f3d780b2c70e913c2c9e622a42eec3
Stéphane BOURDIER <stephane.bourdier@niji.fr>
2025-11-07 16:43:09 +0100

    [BUILD] Add emergent to CORS policy

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,44 @@
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { AppModule } from './app.module';
-import { join } from 'path';
-import * as cookieParser from 'cookie-parser';
+import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { AppModule } from "./app.module";
+import { join } from "path";
+import * as cookieParser from "cookie-parser";
 // import { AllExceptionsFilter } from './exceptions/all-exception.filter';
 // import { UnhandledInterceptor } from './interceptors/unhandled.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // Add AllException Filter to all app
-  // const httpAdapter = app.get(HttpAdapterHost);
-  // app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+	// Add AllException Filter to all app
+	// const httpAdapter = app.get(HttpAdapterHost);
+	// app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
 
-  // Global interceptor
-  // app.useGlobalInterceptors(new UnhandledInterceptor());
+	// Global interceptor
+	// app.useGlobalInterceptors(new UnhandledInterceptor());
 
-  // Add views and public directory
-  app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+	// Add views and public directory
+	app.useStaticAssets(join(__dirname, "..", "public"));
+	app.setBaseViewsDir(join(__dirname, "..", "views"));
 
-  // Template engine is handlebars
-  // app.setViewEngine('hbs');
+	// Template engine is handlebars
+	// app.setViewEngine('hbs');
 
-  // Enable CORS https://github.com/expressjs/cors#configuration-options
-  app.enableCors({
+	// Enable CORS https://github.com/expressjs/cors#configuration-options
+	app.enableCors({
 		origin: [
+			"http://127.0.0.1:4200/",
+			"http://localhost:4200/",
 			"http://127.0.0.1:3001",
 			"http://localhost:3001",
-      "https://gamecal.sb-pro.fr"
+			"https://gamecal.sb-pro.fr",
+			"https://gamecalendar.preview.emergentagent.com",
 		],
 		credentials: true,
-  });
+	});
 
-  // Unlock the request.cookies power
-  app.use(cookieParser());
+	// Unlock the request.cookies power
+	app.use(cookieParser());
 
-  await app.listen(3000);
+	await app.listen(3000);
 }
 bootstrap();
